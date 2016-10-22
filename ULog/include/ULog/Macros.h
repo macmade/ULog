f9b33b0a04e3b779d10aa97c159316c021413789
macmade <macmade@xs-labs.com>
2016-10-22 15:48:42 +0200

    Macros don't require arguments anymore.

--- ULog/include/ULog/Macros.h
+++ ULog/include/ULog/Macros.h
@@ -32,63 +32,63 @@
 
 #if defined( ULOG_DISABLE ) && ULOG_DISABLE == 1
 
-#define ULog( _fmt_, ... )              
-#define ULogEmergency( _fmt_, ... )     
-#define ULogAlert( _fmt_, ... )         
-#define ULogCritical( _fmt_, ... )      
-#define ULogError( _fmt_, ... )         
-#define ULogWarning( _fmt_, ... )       
-#define ULogNotice( _fmt_, ... )        
-#define ULogInfo( _fmt_, ... )          
-#define ULogDebug( _fmt_, ... )         
+#define ULog( ... )             
+#define ULogEmergency( ... )    
+#define ULogAlert( ... )        
+#define ULogCritical( ... )     
+#define ULogError( ... )        
+#define ULogWarning( ... )      
+#define ULogNotice( ... )       
+#define ULogInfo( ... )         
+#define ULogDebug( ... )        
 
 #elif defined( __cplusplus ) && defined( __OBJC__ )
 
-#define ULog( _fmt_, ... )              [ [ ULogLogger sharedInstance ] logWithSource: ULogMessageSourceOBJCXX level: ULogMessageLevelDebug format: _fmt_, __VA_ARGS__ ]
-#define ULogEmergency( _fmt_, ... )     [ [ ULogLogger sharedInstance ] emergencyWithSource: ULogMessageSourceOBJCXX format: _fmt_, __VA_ARGS__ ]
-#define ULogAlert( _fmt_, ... )         [ [ ULogLogger sharedInstance ] alertWithSource: ULogMessageSourceOBJCXX format: _fmt_, __VA_ARGS__ ]
-#define ULogCritical( _fmt_, ... )      [ [ ULogLogger sharedInstance ] criticalWithSource: ULogMessageSourceOBJCXX format: _fmt_, __VA_ARGS__ ]
-#define ULogError( _fmt_, ... )         [ [ ULogLogger sharedInstance ] errorWithSource: ULogMessageSourceOBJCXX format: _fmt_, __VA_ARGS__ ]
-#define ULogWarning( _fmt_, ... )       [ [ ULogLogger sharedInstance ] warningWithSource: ULogMessageSourceOBJCXX format: _fmt_, __VA_ARGS__ ]
-#define ULogNotice( _fmt_, ... )        [ [ ULogLogger sharedInstance ] noticeWithSource: ULogMessageSourceOBJCXX format: _fmt_, __VA_ARGS__ ]
-#define ULogInfo( _fmt_, ... )          [ [ ULogLogger sharedInstance ] infoWithSource: ULogMessageSourceOBJCXX format: _fmt_, __VA_ARGS__ ]
-#define ULogDebug( _fmt_, ... )         [ [ ULogLogger sharedInstance ] debugWithSource: ULogMessageSourceOBJCXX format: _fmt_, __VA_ARGS__ ]
+#define ULog( ... )             [ [ ULogLogger sharedInstance ] logWithSource: ULogMessageSourceOBJCXX level: ULogMessageLevelDebug format: __VA_ARGS__ ]
+#define ULogEmergency( ... )    [ [ ULogLogger sharedInstance ] emergencyWithSource: ULogMessageSourceOBJCXX format: __VA_ARGS__ ]
+#define ULogAlert( ... )        [ [ ULogLogger sharedInstance ] alertWithSource: ULogMessageSourceOBJCXX format: __VA_ARGS__ ]
+#define ULogCritical( ... )     [ [ ULogLogger sharedInstance ] criticalWithSource: ULogMessageSourceOBJCXX format: __VA_ARGS__ ]
+#define ULogError( ... )        [ [ ULogLogger sharedInstance ] errorWithSource: ULogMessageSourceOBJCXX format: __VA_ARGS__ ]
+#define ULogWarning( ... )      [ [ ULogLogger sharedInstance ] warningWithSource: ULogMessageSourceOBJCXX format: __VA_ARGS__ ]
+#define ULogNotice( ... )       [ [ ULogLogger sharedInstance ] noticeWithSource: ULogMessageSourceOBJCXX format: __VA_ARGS__ ]
+#define ULogInfo( ... )         [ [ ULogLogger sharedInstance ] infoWithSource: ULogMessageSourceOBJCXX format: __VA_ARGS__ ]
+#define ULogDebug( ... )        [ [ ULogLogger sharedInstance ] debugWithSource: ULogMessageSourceOBJCXX format: __VA_ARGS__ ]
 
 #elif defined( __cplusplus )
 
-#define ULog( _fmt_, ... )              ULog::Logger::SharedInstance()->Log( ULog::Message::SourceCXX, ULog::Message::LevelDebug, _fmt_, __VA_ARGS__ )
-#define ULogEmergency( _fmt_, ... )     ULog::Logger::SharedInstance()->Emergency( ULog::Message::SourceCXX, _fmt_, __VA_ARGS__ )
-#define ULogAlert( _fmt_, ... )         ULog::Logger::SharedInstance()->Alert( ULog::Message::SourceCXX, _fmt_, __VA_ARGS__ )
-#define ULogCritical( _fmt_, ... )      ULog::Logger::SharedInstance()->Critical( ULog::Message::SourceCXX, _fmt_, __VA_ARGS__ )
-#define ULogError( _fmt_, ... )         ULog::Logger::SharedInstance()->Error( ULog::Message::SourceCXX, _fmt_, __VA_ARGS__ )
-#define ULogWarning( _fmt_, ... )       ULog::Logger::SharedInstance()->Warning( ULog::Message::SourceCXX, _fmt_, __VA_ARGS__ )
-#define ULogNotice( _fmt_, ... )        ULog::Logger::SharedInstance()->Notice( ULog::Message::SourceCXX, _fmt_, __VA_ARGS__ )
-#define ULogInfo( _fmt_, ... )          ULog::Logger::SharedInstance()->Info( ULog::Message::SourceCXX, _fmt_, __VA_ARGS__ )
-#define ULogDebug( _fmt_, ... )         ULog::Logger::SharedInstance()->Debug( ULog::Message::SourceCXX, _fmt_, __VA_ARGS__ )      
+#define ULog( ... )             ULog::Logger::SharedInstance()->Log( ULog::Message::SourceCXX, ULog::Message::LevelDebug, __VA_ARGS__ )
+#define ULogEmergency( ... )    ULog::Logger::SharedInstance()->Emergency( ULog::Message::SourceCXX, __VA_ARGS__ )
+#define ULogAlert( ... )        ULog::Logger::SharedInstance()->Alert( ULog::Message::SourceCXX, __VA_ARGS__ )
+#define ULogCritical( ... )     ULog::Logger::SharedInstance()->Critical( ULog::Message::SourceCXX, __VA_ARGS__ )
+#define ULogError( ... )        ULog::Logger::SharedInstance()->Error( ULog::Message::SourceCXX, __VA_ARGS__ )
+#define ULogWarning( ... )      ULog::Logger::SharedInstance()->Warning( ULog::Message::SourceCXX, __VA_ARGS__ )
+#define ULogNotice( ... )       ULog::Logger::SharedInstance()->Notice( ULog::Message::SourceCXX, __VA_ARGS__ )
+#define ULogInfo( ... )         ULog::Logger::SharedInstance()->Info( ULog::Message::SourceCXX, __VA_ARGS__ )
+#define ULogDebug( ... )        ULog::Logger::SharedInstance()->Debug( ULog::Message::SourceCXX, __VA_ARGS__ )      
 
 #elif defined( __OBJC__ )
 
-#define ULog( _fmt_, ... )              [ [ ULogLogger sharedInstance ] logWithSource: ULogMessageSourceOBJC level: ULogMessageLevelDebug format: _fmt_, __VA_ARGS__ ]
-#define ULogEmergency( _fmt_, ... )     [ [ ULogLogger sharedInstance ] emergencyWithSource: ULogMessageSourceOBJC format: _fmt_, __VA_ARGS__ ]
-#define ULogAlert( _fmt_, ... )         [ [ ULogLogger sharedInstance ] alertWithSource: ULogMessageSourceOBJC format: _fmt_, __VA_ARGS__ ]
-#define ULogCritical( _fmt_, ... )      [ [ ULogLogger sharedInstance ] criticalWithSource: ULogMessageSourceOBJC format: _fmt_, __VA_ARGS__ ]
-#define ULogError( _fmt_, ... )         [ [ ULogLogger sharedInstance ] errorWithSource: ULogMessageSourceOBJC format: _fmt_, __VA_ARGS__ ]
-#define ULogWarning( _fmt_, ... )       [ [ ULogLogger sharedInstance ] warningWithSource: ULogMessageSourceOBJC format: _fmt_, __VA_ARGS__ ]
-#define ULogNotice( _fmt_, ... )        [ [ ULogLogger sharedInstance ] noticeWithSource: ULogMessageSourceOBJC format: _fmt_, __VA_ARGS__ ]
-#define ULogInfo( _fmt_, ... )          [ [ ULogLogger sharedInstance ] infoWithSource: ULogMessageSourceOBJC format: _fmt_, __VA_ARGS__ ]
-#define ULogDebug( _fmt_, ... )         [ [ ULogLogger sharedInstance ] debugWithSource: ULogMessageSourceOBJC format: _fmt_, __VA_ARGS__ ]
+#define ULog( ... )             [ [ ULogLogger sharedInstance ] logWithSource: ULogMessageSourceOBJC level: ULogMessageLevelDebug format: __VA_ARGS__ ]
+#define ULogEmergency( ... )    [ [ ULogLogger sharedInstance ] emergencyWithSource: ULogMessageSourceOBJC format: __VA_ARGS__ ]
+#define ULogAlert( ... )        [ [ ULogLogger sharedInstance ] alertWithSource: ULogMessageSourceOBJC format: __VA_ARGS__ ]
+#define ULogCritical( ... )     [ [ ULogLogger sharedInstance ] criticalWithSource: ULogMessageSourceOBJC format: __VA_ARGS__ ]
+#define ULogError( ... )        [ [ ULogLogger sharedInstance ] errorWithSource: ULogMessageSourceOBJC format: __VA_ARGS__ ]
+#define ULogWarning( ... )      [ [ ULogLogger sharedInstance ] warningWithSource: ULogMessageSourceOBJC format: __VA_ARGS__ ]
+#define ULogNotice( ... )       [ [ ULogLogger sharedInstance ] noticeWithSource: ULogMessageSourceOBJC format: __VA_ARGS__ ]
+#define ULogInfo( ... )         [ [ ULogLogger sharedInstance ] infoWithSource: ULogMessageSourceOBJC format: __VA_ARGS__ ]
+#define ULogDebug( ... )        [ [ ULogLogger sharedInstance ] debugWithSource: ULogMessageSourceOBJC format: __VA_ARGS__ ]
 
 #else
 
-#define ULog( _fmt_, ... )              ULog_LogWithLevel( ULog_Message_LevelDebug, _fmt_, __VA_ARGS__ )
-#define ULogEmergency( _fmt_, ... )     ULog_Emergency( _fmt_, __VA_ARGS__ )
-#define ULogAlert( _fmt_, ... )         ULog_Alert( _fmt_, __VA_ARGS__ )
-#define ULogCritical( _fmt_, ... )      ULog_Critical( _fmt_, __VA_ARGS__ )
-#define ULogError( _fmt_, ... )         ULog_Error( _fmt_, __VA_ARGS__ )
-#define ULogWarning( _fmt_, ... )       ULog_Warning( _fmt_, __VA_ARGS__ )
-#define ULogNotice( _fmt_, ... )        ULog_Notice( _fmt_, __VA_ARGS__ )
-#define ULogInfo( _fmt_, ... )          ULog_Info( _fmt_, __VA_ARGS__ )
-#define ULogDebug( _fmt_, ... )         ULog_Debug( _fmt_, __VA_ARGS__ )
+#define ULog( ... )             ULog_LogWithLevel( ULog_Message_LevelDebug, __VA_ARGS__ )
+#define ULogEmergency( ... )    ULog_Emergency( __VA_ARGS__ )
+#define ULogAlert( ... )        ULog_Alert( __VA_ARGS__ )
+#define ULogCritical( ... )     ULog_Critical( __VA_ARGS__ )
+#define ULogError( ... )        ULog_Error( __VA_ARGS__ )
+#define ULogWarning( ... )      ULog_Warning( __VA_ARGS__ )
+#define ULogNotice( ... )       ULog_Notice( __VA_ARGS__ )
+#define ULogInfo( ... )         ULog_Info( __VA_ARGS__ )
+#define ULogDebug( ... )        ULog_Debug( __VA_ARGS__ )
 
 #endif
 
