79fefe4474fd40e54abad28408f779b521d27850
Jean-David Gadina <macmade@xs-labs.com>
2016-10-27 16:54:45 +0200

    Preparing for Windows build...

---
+++ Example-Win/main.cpp
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2016 Jean-David Gadina - www-xs-labs.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+/*!
+ * @file        main.cpp
+ * @copyright   (c) 2016, Jean-David Gadina - www.xs-labs.com
+ */
+
+#include "stdafx.h"
+#include <iostream>
+#include <thread>
+#include <Windows.h>
+
+static void start( void );
+static void start( void )
+{
+    while( 1 )
+    {
+        Sleep( 500 );
+    }
+}
+
+int main( void )
+{
+    std::cerr << "ULog Example - Win"
+              << std::endl
+              << std::endl
+              << " - Press any key to start logging."
+              << std::endl
+              << " - Then press any key to exit."
+              << std::endl
+              << std::endl;
+
+    getchar();
+
+    {
+        std::thread t
+        (
+            []()
+            {
+                start();
+            }
+        );
+
+        t.detach();
+    }
+
+    getchar();
+
+    return 0;
+}
+
